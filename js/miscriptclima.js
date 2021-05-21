7877995df7143f099db2e46e5235da08d971f3db
Jose0413 <jose.sepulveda0413@gmail.com>
2021-05-20 20:40:50 -0400

    y

---
+++ js/miscriptclima.js
@@ -0,0 +1,38 @@
+// JQUERY
+//$(document).ready(function(){
+//    $('#datosciudad').hide();
+//});
+
+// JS
+function buscar(){
+    
+    // JS
+
+    // VARIABLES
+    var clima = new XMLHttpRequest();
+    var ciudad = document.getElementById("ciudad").value;
+    
+    // API
+    clima.open('GET','https://api.openweathermap.org/data/2.5/weather?q=' + ciudad + '&appid=8fefcdc4f65e87fdd7c08f9e34215dc5&units=metric', false);
+    clima.send(null);
+
+    // DATOS
+    var datos = JSON.parse(clima.response);
+    var ciudad = datos.name;
+    var temperatura = datos.main.temp_min;
+    var humedad = datos.main.humidity;
+    var velocidadviento = datos.wind.speed;
+
+    // JQUERY
+
+    // DISTRIBUIR DATOS
+    $('#ubicacion').html(ciudad);
+    $('#temperatura').html(temperatura);
+    $('#humedad').html(humedad);
+    $('#velocidadviento').html(velocidadviento);
+
+    //$('#ciudad').val('');
+    //$('#climaconsulta').show();
+
+}
+
